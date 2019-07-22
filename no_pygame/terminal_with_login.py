b1917cb667bb301ebc8310355d9518478d0ea24e
Rowan-McAlpin <37227254+Rowan-McAlpin@users.noreply.github.com>
2019-07-22 14:53:08 -0700

    Update terminal_with_login.py

--- no_pygame/terminal_with_login.py
+++ no_pygame/terminal_with_login.py
@@ -20,56 +20,75 @@ def login(data):
         print('Username incorrect. Please try again.')
         return False
 
-
-def command(data):
-    input_command = input(data['username'] + "@pythonterminal:~ $ ")
-    if input_command == "exit":
-        quit()
-        
-    elif input_command == "print-data":
-        print(" Your name is " + data['username'] + ".")
-        
-    elif input_command == "help":
-        print(" The commands you can use are:")
-        print("     exit - quit the terminal - This doesn't work in MU 1.0.2.")
-        print("     help - opens the help page")
-        print("     print-data - View your data")
-        print("     change-user - Change the username")
-        print("     change-password - Change the password")
-        print("     sudo change-user - Change the username, but this time it'll work")
-        print("     sudo change-password - Change the password, but this time it'll work")
-
-    elif input_command == "":
-        return False
-        
-    elif input_command == "change-user":
-        print("Needs to be sudo. Try sudo change-user.")
-
-    elif input_command == "change-password":
-        print("Needs to be sudo. Try sudo change-password.")
+if login(data):
     
-    elif input_command == "sudo change-user":
-        print("This command requires superuser privileges.")
-        if login(data):
-            result = data
-            result['username'] = input(
-                "What would you like the username to be? ")
-            return result
+    def command(data):
+        input_command = input(data['username'] + "@pythonterminal:~ $ ")
+        if input_command == "exit":
+            quit()
+            
+        elif input_command == "print-data":
+            print(" Your name is " + data['username'] + ".")
+            
+        elif input_command == "help":
+            print(" The commands you can use are:")
+            print("     exit - quit the terminal - This doesn't work in MU 1.0.2.")
+            print("     help - opens the help page")
+            print("     print-data - View your data")
+            print("     change-user - Change the username")
+            print("     change-password - Change the password")
+            print("     sudo change-user - Change the username, but this time it'll work")
+            print("     sudo change-password - Change the password, but this time it'll work")
+    
+        elif input_command == "":
+            return False
+            
+        elif input_command == "change-user":
+            print("Needs to be sudo. Try sudo change-user.")
+    
+        elif input_command == "change-password":
+            print("Needs to be sudo. Try sudo change-password.")
+        
+        elif input_command == "sudo change-user":
+            print("This command requires superuser privileges.")
+            if login(data):
+                result = data
+                result['username'] = input(
+                    "What would you like the username to be? ")
+                return result
+            else:
+                print("Sorry, the password is incorrect.")
+        elif input_command == "sudo change-password":
+            print("This command requres superuser privileges.")
+            if login(data):
+                result = data
+                result['password'] = input(
+                    "What would you like the password to be? ")
+                return result
+            else:
+                print("Sorry, the password is incorrect.")
         else:
-            print("Sorry, the password is incorrect.")
-    elif input_command == "sudo change-password":
-        print("This command requres superuser privileges.")
-        if login(data):
-            result = data
-            result['password'] = input(
-                "What would you like the password to be? ")
-            return result
-        else:
-            print("Sorry, the password is incorrect.")
-    else:
-        print(" Bash: " + input_command + ": command not found.")
-    return False
+            print(" Bash: " + input_command + ": command not found.")
+        return False
 
+else:
+    def command(data):
+        input_command = input(data['username'] + "@pythonterminal:~ $ ")
+        if input_command == "exit":
+            quit()
+            
+            
+        elif input_command == "help":
+            print(" The commands you can use are:")
+            print("     exit - quit the terminal - This doesn't work in MU 1.0.2.")
+            print("     help - opens the help page")
+    
+        elif input_command == "":
+            return False
+        
+        else:
+            print(" Bash: " + input_command + ": command not found.")
+        return False
 
 data = {
     'username': 'terminal',
